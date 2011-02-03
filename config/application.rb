cfe4fbab72b0990bbd1cccc8f805c47dd214e1ac
Lucas Dutra Nunes <ldnunes@ossystems.com.br>
2011-02-03 14:42:54 -0200

    Adicionada licença GPL em cada arquivo

--- config/application.rb
+++ config/application.rb
@@ -1,3 +1,22 @@
+# Controle de Horas - Sistema para gestão de horas trabalhadas
+# Copyright (C) 2009  O.S. Systems Softwares Ltda.
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as
+# published by the Free Software Foundation, either version 3 of the
+# License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# Rua Clóvis Gularte Candiota 132, Pelotas-RS, Brasil.
+# e-mail: contato@ossystems.com.br
+
 require File.expand_path('../boot', __FILE__)
 
 require "rails"
@@ -20,32 +39,6 @@ Bundler.require(:default, Rails.env) if defined?(Bundler)
 
 module Orion6Plugin
   class Engine < Rails::Engine
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
-    # Custom directories with classes and modules you want to be autoloadable.
-    # config.autoload_paths += %W(#{config.root}/extras)
-
-    # Only load the plugins named here, in the order given (default is alphabetical).
-    # :all can be used as a placeholder for all plugins not explicitly named.
-    # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-
-    # Activate observers that should always be running.
-    # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-
-    # JavaScript files you want as :defaults (application.js is always included).
-    # config.action_view.javascript_expansions[:defaults] = %w(jquery rails)
-
-    # Configure the default encoding used in templates for Ruby 1.9.
     config.encoding = "utf-8"
   end
 end
